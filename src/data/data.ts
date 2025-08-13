59b6c88c9c83addb4acfa814589cf901f8b2512b
Saronzeleke <Cassiopiasaron@gmail.com>
2025-08-13 14:54:34 +0300

    commit

--- src/data/data.ts
+++ src/data/data.ts
@@ -8,7 +8,7 @@ export const testimonials: Testimonial[] = [
     company: "Abay Mart.",
     content: "It was a real pleasure having Ekram as part of the team. She consistently impressed me with her eagerness to learn, and not only did she meet expectations, she always went above and beyond what was asked of her. Her work was consistently delivered on time, and she played a significant role in managing our social media presence. What really stood out was her humble attitude and willingness to absorb new skills quickly – I truly believe these qualities would make her a great fit for any position, as she has the ability to pick things up rapidly. I highly recommend her and wish her the very best in her future endeavors!",
     rating: 5,
-    image:'/public/tk.jpg'
+    image:'C:\Users\admin\Ekram-portifolio\dist\tk.jpg'
   },
   {
     id: 2,
@@ -17,7 +17,7 @@ export const testimonials: Testimonial[] = [
     company: "StartupHub",
     content: "would love to highly recommend Ekram as a social media manager. She's creative, reliable, and truly understands how to grow and engage an audience. Her strategies brought noticeable improvement to my brand's online presence, and working with her has been a smooth, professional experience.",
     rating: 5,
-    image: "/public/yk.jpg"
+    image: "C:\Users\admin\Ekram-portifolio\dist\yk.jpg"
   },
   // {
   //   id: 3,
@@ -45,7 +45,7 @@ export const certificates: Certificate[] = [
 title: "Ethiopia Digital Marketing Membership",
 issuer:"Ethiopia Digital Marketing",
 date:"2024",
-image:"/public/abal.jpg",
+image:"C:\Users\admin\Ekram-portifolio\dist\abal.jpg",
 credentialId:"EDM-2024-005"
     
   },
@@ -54,7 +54,7 @@ credentialId:"EDM-2024-005"
     title: "Social Media Marketing Certification",
     issuer: "HubSpot",
     date: "2023",
-    image: "./public/c2.jpg",
+    image: "C:\Users\admin\Ekram-portifolio\dist\c2.jpg",
     credentialId: "HS-2023-003"
   },
   {
@@ -62,7 +62,7 @@ credentialId:"EDM-2024-005"
     title: "Benetsa Marketing And communication",
     issuer: "Bentsa",
     date: "2024",
-    image: "/public/c1.jpg",
+    image: "C:\Users\admin\Ekram-portifolio\dist\c1.jpg",
     credentialId: "GA-2024-001"
   },
   {
@@ -70,7 +70,7 @@ credentialId:"EDM-2024-005"
     title: "Digital Marketing Fundamentals",
     issuer: "Fundamentals of Digital Marketing",
     date: "2023",
-    image:"/public/c4.jpg",
+    image:"C:\Users\admin\Ekram-portifolio\dist\c4.jpg",
     credentialId: "AD-2023-004"
   },
   {
@@ -79,7 +79,7 @@ id:5,
     title: "Acomplishment in Digital Marketing",
     issuer: "Merhayan",
     date: "2024",
-    image: "./public/c3.jpg",
+    image: "C:\Users\admin\Ekram-portifolio\dist\c3.jpg",
     credentialId: "FB-2024-002"
   }
 ];
@@ -90,7 +90,7 @@ export const projects: Project[] = [
     title: "Meta Ads campaign",
     category: " Ads Specialist",
     description: "Complete  Meta Ads campaign campaign that increased sales by 300% through strategic social media marketing and targeted advertising.",
-    image: "/public/l7.jpg",
+    image: "C:\Users\admin\Ekram-portifolio\dist\l7.jpg",
     technologies: ["Facebook Ads"]
   },
   {
@@ -98,7 +98,7 @@ export const projects: Project[] = [
     title: "Social media management",
     category: "Social media manager",
     description: "Social media management .",
-    image: "/public/l5.jpg",
+    image: "C:\Users\admin\Ekram-portifolio\dist\l5.jpg",
     technologies: ["LinkedIn Ads"]
   },
   {
@@ -106,7 +106,7 @@ export const projects: Project[] = [
     title: "Copy Writing for Local Business",
     category: "Copywriting",
     description: "Social media strategy and content creation that boosted engagement by 400% and increased foot traffic significantly.",
-    image: "/public/l2.jpg",
+    image: "C:\Users\admin\Ekram-portifolio\dist\l2.jpg",
     technologies: [ "Content Creation", "Analytics"]
   },
   {
@@ -114,7 +114,7 @@ export const projects: Project[] = [
     title: "Social Media Management ",
     category: "Social media manager",
     description: "Social media management ",
-    image: "/public/l6.jpg",
+    image: "C:\Users\admin\Ekram-portifolio\dist\l6.jpg",
     technologies: ["Social Media Management","Content Creation","Analytics"]
   },
   {
@@ -122,7 +122,7 @@ export const projects: Project[] = [
     title: "Social media management",
     category: "  Social media manager",
     description: "Social media management ",
-    image: "/public/l8.jpg",
+    image: "C:\Users\admin\Ekram-portifolio\dist\l8.jpg",
     technologies: ["LinkedIn Ads", "Email Marketing", "CRM", "Analytics"]
   },
   {
@@ -130,7 +130,7 @@ export const projects: Project[] = [
     title: "Creative director for the CSR initiative creative Africa",
     category: "creative manager",
     description:" led the creative direction for a csr initiative that foucesed on empowering local communities through digital marketing and social media.",
-    image: "/public/l1.jpg",
+    image: "C:\Users\admin\Ekram-portifolio\dist\l1.jpg",
     technologies:["Social media management", "Content Creation","creative manager","Analytics"]
   },
   {
@@ -138,7 +138,7 @@ export const projects: Project[] = [
     title:"content writer",
     category:"content writer",
     description:"content writer for abay mart",
-    image:"/public/l3.jpg",
+    image:"C:\Users\admin\Ekram-portifolio\dist\l3.jpg",
     technologies:["content writing"]
   }
 ];
